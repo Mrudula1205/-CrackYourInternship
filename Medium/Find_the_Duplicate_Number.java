188a975287141bdd621289a10d626ba0e592fb06
unknown <mrudula12523@gmail.com>
2024-07-14 22:34:16 +0400

    commit modifications

--- Medium/Find_the_Duplicate_Number.java
+++ Medium/Find_the_Duplicate_Number.java
@@ -11,3 +11,20 @@ class Solution {
         return dup;
     }
 }
+
+/*
+ * Brute Force Approach : time complexity O(n^2)
+ * Sortig and searching : time complexity O(nlogn)
+ * Using Hashing : time complexity O(n) and space complexity O(n)
+ * Floyd 
+ * Convert the array into linked list and find the cycle
+ * Fast and slow pointer
+ * slow = nums[slow]
+ * fast = nums[nums[fast]]
+ * till slow and fast meet
+ * 
+ * them slow = nums[0]
+ * then move slow and fast one step at a time
+ * when they meet that is the duplicate number
+ * return slow
+ */
